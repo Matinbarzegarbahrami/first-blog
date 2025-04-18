e7334a3b736481e88f12b7cc52002bdab29d6e40
Matinbarzegar <matinbarzegarbahrami84@gmail.com>
2025-04-18 20:05:23 +0330

    blog project

--- myblog/urls.py
+++ myblog/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # این مسیر برای ادمین است
     path('', include('blog.urls', namespace='blog')),  # مسیرهای اپلیکیشن blog
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
