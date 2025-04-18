afe2038d76a5b6e788f5beeb51e987b509a1846c
Matinbarzegar <matinbarzegarbahrami84@gmail.com>
2025-04-18 19:12:34 +0330

    blog project

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p#!luwxo@$)&)d8z28p#zo)sk%51$iouv8ek90-4p=yyioz)3z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['first-blog-a486.onrender.com']
 
 
 # Application definition
