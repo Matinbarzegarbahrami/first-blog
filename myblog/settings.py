6b8bca748f99e9e068181c02a6ce78d12e12cbf9
Matinbarzegar <matinbarzegarbahrami84@gmail.com>
2025-04-18 20:33:09 +0330

    blog project

--- myblog/settings.py
+++ myblog/settings.py
@@ -77,23 +77,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myblog_db',
-        'USER': 'bloguser',
-        'PASSWORD': 'aAaAs11_c',
-        'HOST': 'first-blog-at6r.onrender.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'myblog_db',
+#         'USER': 'bloguser',
+#         'PASSWORD': 'aAaAs11_c',
+#         'HOST': 'first-blog-at6r.onrender.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
